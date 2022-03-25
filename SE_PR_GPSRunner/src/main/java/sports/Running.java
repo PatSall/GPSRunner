dc885c4f7a0fb3d969b8d686c59061a1140246aa
SusiGu <102374328+SusiGu@users.noreply.github.com>
2022-03-25 21:25:06 +0100

    add maven

--- SE_PR_GPSRunner/src/main/java/sports/Running.java
+++ SE_PR_GPSRunner/src/main/java/sports/Running.java
@@ -1,5 +1,8 @@
 package sports;
 
 public class Running {
-
+    public static void main(String[] args) {
+        System.out.println("Susi is programming");
+        System.out.println("It was so easy");
+    }
 }
