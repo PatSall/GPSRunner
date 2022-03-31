d923e0254e0d99773cdde5763901914dfa731b18
SusiGu <102374328+SusiGu@users.noreply.github.com>
2022-03-31 23:20:22 +0200

    add Controller Activity not finish

---
+++ SE_PR_GPSRunner/src/main/java/sports/Activity.java
@@ -0,0 +1,165 @@
+package sports;
+
+import jakarta.xml.bind.annotation.*;
+
+import java.sql.Timestamp;
+
+
+// order of the fields in XML
+/*@XmlType(propOrder = {"sport", "destination", "startTime",
+                "totalTimeSeconds", "distanceMeters",
+        "maximumSpeed","calories" , "averageHeartRateBpm",
+        "maximumHeartRateBpm", "intensity", "triggerMethod"
+})*/
+//@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Activity {
+
+    private String sport;
+
+    private String destination;
+
+    private String startTime;
+
+    private Double totalTimeSeconds;
+
+    private Double distanceMeters;
+
+    private Double maximumSpeed;
+
+    private Integer calories;
+
+    private Integer averageHeartRateBpm;
+
+    private Integer maximumHeartRateBpm;
+
+    private String intensity;
+
+    private String triggerMethod;
+
+    public Activity() {
+
+    }
+
+    public Activity(String sport, String destination, String startTime, Double totalTimeSeconds, Double distanceMeters, Double maximumSpeed, Integer calories, Integer averageHeartRateBpm, Integer maximumHeartRateBpm, String intensity, String triggerMethod) {
+        this.sport = sport;
+        this.destination = destination;
+        this.startTime = startTime;
+        this.totalTimeSeconds = totalTimeSeconds;
+        this.distanceMeters = distanceMeters;
+        this.maximumSpeed = maximumSpeed;
+        this.calories = calories;
+        this.averageHeartRateBpm = averageHeartRateBpm;
+        this.maximumHeartRateBpm = maximumHeartRateBpm;
+        this.intensity = intensity;
+        this.triggerMethod = triggerMethod;
+    }
+
+    @XmlAttribute
+    public String getSport() {
+        return sport;
+    }
+    @XmlElement(name = "Id")
+    public String getDestination() {
+        return destination;
+    }
+   @XmlElement(name = "Lap")
+    public String getStartTime() {
+        return startTime;
+    }
+
+    @XmlElement(name = "TotalTimeSeconds")
+    public Double getTotalTimeSeconds() {
+        return totalTimeSeconds;
+    }
+
+    @XmlElement(name = "DistanceMeters")
+    public Double getDistanceMeters() {
+        return distanceMeters;
+    }
+
+
+    @XmlElement(name = "MaximumSpeed")
+    public Double getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    @XmlElement(name = "Calories")
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public Integer getAverageHeartRateBpm() {
+        return averageHeartRateBpm;
+    }
+
+    @XmlElement(name = "MaximumHeartRateBpm")
+    public Integer getMaximumHeartRateBpm() {
+        return maximumHeartRateBpm;
+    }
+
+    @XmlElement(name = "Intensity")
+    public String getIntensity() {
+        return intensity;
+    }
+
+    @XmlElement(name = "TriggerMethod")
+    public String getTriggerMethod() {
+        return triggerMethod;
+    }
+
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+
+    public void setTotalTimeSeconds(Double totalTimeSeconds) {
+        this.totalTimeSeconds = totalTimeSeconds;
+    }
+
+
+    public void setDistanceMeters(Double distanceMeters) {
+        this.distanceMeters = distanceMeters;
+    }
+
+
+    public void setMaximumSpeed(Double maximumSpeed) {
+        this.maximumSpeed = maximumSpeed;
+    }
+
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    @XmlElement(name = "AverageHeartRateBpm")
+    public void setAverageHeartRateBpm(Integer averageHeartRateBpm) {
+        this.averageHeartRateBpm = averageHeartRateBpm;
+    }
+
+
+    public void setMaximumHeartRateBpm(Integer maximumHeartRateBpm) {
+        this.maximumHeartRateBpm = maximumHeartRateBpm;
+    }
+
+
+    public void setIntensity(String intensity) {
+        this.intensity = intensity;
+    }
+
+
+    public void setTriggerMethod(String triggerMethod) {
+        this.triggerMethod = triggerMethod;
+    }
+}
--- SE_PR_GPSRunner/src/main/java/sports/Controller.java
+++ SE_PR_GPSRunner/src/main/java/sports/Controller.java
@@ -1,8 +1,41 @@
 package sports;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.File;
+
 public class Controller {
     public static void main(String[] args) {
         System.out.println("READ XML File with JAXB");
+
+
+        //JAXBContext jaxbContext = null;
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Activity.class);
+
+            File file = new File("C:\\temp\\activity.xml");
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Activity sportData = (Activity) jaxbUnmarshaller.unmarshal(file);
+
+            //List<Fruit>  fits = sportData.getId();
+
+            System.out.println(sportData);
+            System.out.println(sportData.getSport());
+            System.out.println(sportData.getCalories());
+            System.out.println(sportData.getDestination());
+            System.out.println(sportData.getIntensity());
+            System.out.println(sportData.getMaximumHeartRateBpm());
+            System.out.println(sportData.getTotalTimeSeconds());
+            System.out.println(sportData.getAverageHeartRateBpm());
+            System.out.println(sportData.getStartTime());
+
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
 
