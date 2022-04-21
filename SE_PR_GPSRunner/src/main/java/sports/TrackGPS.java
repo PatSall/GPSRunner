6773a03c828210e8ac3ac11757feb570d2e28ff4
SusiGu <102374328+SusiGu@users.noreply.github.com>
2022-04-21 17:58:14 +0200

    Parse gpx and txc

---
+++ SE_PR_GPSRunner/src/main/java/sports/TrackGPS.java
@@ -0,0 +1,57 @@
+package sports;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class TrackGPS {
+
+    private String name;
+    private Date date;
+    private List<TrackSegment> trackSegments;
+
+
+    public TrackGPS() {}
+
+    public TrackGPS(String name, Date date, List<TrackSegment> trackSegments) {
+        this.name = name;
+        this.date = date;
+        this.trackSegments = trackSegments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<TrackSegment> getTrackSegments() {
+        return trackSegments;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(Date  date) {
+        this.date = date;
+    }
+
+    public void setTrackSegments(List<TrackSegment> trackSegments) {
+        this.trackSegments = trackSegments;
+    }
+
+    @Override
+    public String toString() {
+        return "TrackGPS{" +
+                "name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", trackSegment=" + trackSegments +
+                '}';
+    }
+
+
+}
---
+++ SE_PR_GPSRunner/src/main/java/sports/TrackSegment.java
@@ -0,0 +1,59 @@
+package sports;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+public class TrackSegment extends TrackGPS {
+
+    private Double trackPointLat;
+    private Double trackPointLon;
+    private Double elem;
+    private LocalDateTime time;
+
+    public TrackSegment () {
+        super();
+    }
+
+    public Double getTrackPointLat() {
+        return trackPointLat;
+    }
+
+    public void setTrackPointLat(Double trackPointLat) {
+        this.trackPointLat = trackPointLat;
+    }
+
+    public Double getTrackPointLon() {
+        return trackPointLon;
+    }
+
+    public void setTrackPointLon(Double trackPointLon) {
+        this.trackPointLon = trackPointLon;
+    }
+
+    public Double getElem() {
+        return elem;
+    }
+
+    public void setElem(Double elem) {
+        this.elem = elem;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return  "TrackSegment{" +
+                "trackPointLat=" + trackPointLat +
+                ", trackPointLon=" + trackPointLon +
+                ", elem=" + elem +
+                ", time=" + time +
+                '}';
+    }
+}
