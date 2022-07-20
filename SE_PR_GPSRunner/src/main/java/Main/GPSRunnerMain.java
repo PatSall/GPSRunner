4936499ea425638ad9f867159de4281d8b97d02f
SusiGu <102374328+SusiGu@users.noreply.github.com>
2022-07-20 20:52:39 +0200

    JavaDoc

--- SE_PR_GPSRunner/src/main/java/Main/GPSRunnerMain.java
+++ SE_PR_GPSRunner/src/main/java/Main/GPSRunnerMain.java
@@ -8,13 +8,12 @@ import gui.GUI;
 public class GPSRunnerMain {
 
 	public static void main(String[] args) {
-		// Variable merken und aufrufe überflüssig
+
 		Controller controller = new Controller(new ActivityList(), new GUI());
 		controller.getView().setController(controller);
 		controller.getView().setVisible(true);
 		controller.getView().updateChart(controller.getActivities().getActivities());
 		controller.getView().refreshGui();
-		// Controller ist noc nciht ferrig mit refresh GUI
 		controller.getActivities().readFiles();
 
 
